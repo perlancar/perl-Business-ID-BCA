ecddb7af7985ba649d85c383aab8c732073367f3
perlancar (@pc-office) <perlancar@gmail.com>
2015-09-15 14:45:41 +0700

    Update to newer Perinci::Sub::Gen::AccessTable (default_fields must be array)

--- lib/Business/ID/BCA.pm
+++ lib/Business/ID/BCA.pm
@@ -1020,7 +1020,7 @@ my $res = gen_read_table_func(
     name => 'list_bca_branches',
     table_data => $data,
     table_spec => $meta,
-    default_fields => 'code',
+    default_fields => ['code'],
     #langs => ['en_US', 'id_ID'],
 );
 die "BUG: Can't generate func: $res->[0] - $res->[1]" unless $res->[0] == 200;
